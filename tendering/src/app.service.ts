7b1c35f74f43dda7d488212b45870319f59e9892
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-05 12:31:53 +0300

    feat: add workspace

--- tendering/src/app.service.ts
+++ tendering/src/app.service.ts
@@ -1,7 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import * as ampq from 'amqplib/callback_api';
 
 @Injectable()
 export class AppService {
+  constructor() {
+    const exchange = 'exchange';
+    const receiverQueue = 'publisher-queue';
+    const publisherQueue = 'receiver-queue';
+
+    const receivingRoutingKey = '#';
+
+    const message = 'From workspace to planning';
+
+    ampq.connect('amqp://localhost', function (err, conn) {
+      // receiver channel
+      conn.createChannel(function (err, ch) {
+        ch.assertExchange(exchange, 'topic', {
+          durable: true,
+          autoDelete: false,
+        });
+
+        ch.assertQueue(receiverQueue, { durable: true, autoDelete: false });
+        console.log(
+          ' [*] Waiting for messages in %s. To exit press CTRL+C',
+          receiverQueue,
+        );
+
+        ch.bindQueue(receiverQueue, exchange, receivingRoutingKey);
+
+        ch.consume(
+          receiverQueue,
+          function (msg) {
+            console.log(' [x] Received %s', msg.content.toString());
+          },
+          { noAck: false },
+        );
+      });
+
+      // sender channel
+      conn.createChannel(function (err, ch) {
+        ch.assertExchange(exchange, 'topic', {
+          durable: true,
+          autoDelete: false,
+        });
+
+        ch.assertQueue(publisherQueue, { durable: true, autoDelete: false });
+
+        ch.publish(exchange, 'planning.post', Buffer.from(message));
+
+        console.log("Workspace sent '%s'", message);
+      });
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
